a1814e4d44fdbc2799dbccf3c4e062288e516861
anshyyy <anshuman9998@gmail.com>
2023-05-08 23:53:05 +0530

    socket io setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const {PORT} = require('./config/index');
+
+app.use('/',express.static( __dirname+"/public"));
+
+io.on("connection",(socket)=> {
+    console.log("a user is connected  "+socket.id)
+});
 
 
 const setUpAndStart =() => {
-    app.listen(3000,()=>{
-        console.log("server started at 3000");
+    
+   
+
+    server.listen(PORT,()=>{
+        console.log(`server started at ${PORT}`);
     })
 }
 setUpAndStart();
